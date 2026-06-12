46f5aeda108339716221ae64c70ee79e52b77921
vishal76kd <vishal76kd@gmail.com>
2026-06-12 14:56:52 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'easy-trading-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
